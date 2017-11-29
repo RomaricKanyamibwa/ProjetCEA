f9125afe0b9f7c7f014bf3bb51e3e74902eeb982
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2017-11-29 17:39:40 +0100

    Add files via upload

--- Fusion_de_maillages/square.py
+++ Fusion_de_maillages/square.py
@@ -151,35 +151,76 @@ def square3(k,fig=3):
         listp=square2(k,3)
         return listp
     plt.figure(fig)
-    n=3
-    nodes=list(range(0,n));
-    listp=[]
     
-    #decoupage du carré
-    for j in nodes:
+    if(k==4):
+        n=(k-1)//2+1
+        nodes=list(range(0,n));
+        listp=[]
         
-        if((([n-1,j],[0,j]) not in listp) and (n-j!=0) and (([0,j],[n-1,j]) not in listp)):
-            x = [n-1,0]
-            plt.plot(x,[j,j])
-            listp+=[([n-1,j],[0,j])]
+        #decoupage du carré
+        for j in nodes:
             
-        if(([0,0],[0,j]) not in listp  and (j!=0) and (([0,j],[0,0]) not in listp)):
-            x = [0,0]
-            plt.plot(x,[0,j])
-            #if([0,0] not in listp):
-            listp+=[([0,0],[0,j])]
+            if((([n-1,j],[0,j]) not in listp) and (n-j!=0) and (([0,j],[n-1,j]) not in listp)):
+                x = [n-1,0]
+                plt.plot(x,[j,j])
+                listp+=[([n-1,j],[0,j])]
+                
+            if(([0,0],[0,j]) not in listp  and (j!=0) and (([0,j],[0,0]) not in listp)):
+                x = [0,0]
+                plt.plot(x,[0,j])
+                #if([0,0] not in listp):
+                listp+=[([0,0],[0,j])]
+                
+            if(([0,0],[n-1,0]) not in listp  and (n-1!=0) and (([n-1,0],[0,0]) not in listp )):   
+                x = [0,n-1]
+                plt.plot(x,[0,0])
+                #if([n-1,0] not in listp):
+                listp+=[([0,0],[n-1,0])]
+                
+            if(([n-1,0],[n-1,j]) not in listp  and (j!=0) and (([n-1,j],[n-1,0]) not in listp)):
+                x = [n-1,n-1]
+                plt.plot(x,[0,j])
+                listp+=[([n-1,0],[n-1,j])]
+        triang=0
+        x = [0,j]
+        j=1
+        plt.plot(x,[j-1,j])
+        listp+=[([0,j-1],[j,j])]
+        triang+=1
+        x = [j,0]
+        plt.plot(x,[j-1,j])
+        listp+=[([j,j-1],[0,j])]
+        triang+=1
+    else:
+        n=3
+        nodes=list(range(0,n));
+        listp=[]
+        
+        #decoupage du carré
+        for j in nodes:
             
-        if(([0,0],[n-1,0]) not in listp  and (n-1!=0) and (([n-1,0],[0,0]) not in listp )):   
-            x = [0,n-1]
-            plt.plot(x,[0,0])
-            #if([n-1,0] not in listp):
-            listp+=[([0,0],[n-1,0])]
-            
-        if(([n-1,0],[n-1,j]) not in listp  and (j!=0) and (([n-1,j],[n-1,0]) not in listp)):
-            x = [n-1,n-1]
-            plt.plot(x,[0,j])
-            listp+=[([n-1,0],[n-1,j])]
-    triang=0
+            if((([n-1,j],[0,j]) not in listp) and (n-j!=0) and (([0,j],[n-1,j]) not in listp)):
+                x = [n-1,0]
+                plt.plot(x,[j,j])
+                listp+=[([n-1,j],[0,j])]
+                
+            if(([0,0],[0,j]) not in listp  and (j!=0) and (([0,j],[0,0]) not in listp)):
+                x = [0,0]
+                plt.plot(x,[0,j])
+                #if([0,0] not in listp):
+                listp+=[([0,0],[0,j])]
+                
+            if(([0,0],[n-1,0]) not in listp  and (n-1!=0) and (([n-1,0],[0,0]) not in listp )):   
+                x = [0,n-1]
+                plt.plot(x,[0,0])
+                #if([n-1,0] not in listp):
+                listp+=[([0,0],[n-1,0])]
+                
+            if(([n-1,0],[n-1,j]) not in listp  and (j!=0) and (([n-1,j],[n-1,0]) not in listp)):
+                x = [n-1,n-1]
+                plt.plot(x,[0,j])
+                listp+=[([n-1,0],[n-1,j])]
+        triang=0
 #    nodes=list(range(1,n));
 #    for j in nodes:
 #        if(([0,j-1],[n-1,j]) not in listp and ([n-1,j],[0,j-1]) not in listp):   
@@ -200,10 +241,47 @@ def square3(k,fig=3):
     plt.show()
 
 
-square(3)
-square2(3)
-square3(4)
+#square(3)
+#square2(3)
+#square3(4)
 
+
+def regular_polygon(n,r=1,c1=1,c2=1):
+    listp=[]
+    for i in range(n):
+        listp.append((r*cos(2*pi*i/n)+c1,r*sin(2*pi*i/n)+c2))
+        
+    for i in range (n):
+        p1=listp[i]
+        if(i!=n-1):
+            p2=listp[i+1]
+        else:
+            p2=listp[0]
+        plt.plot([p1[0],p2[0]],[p1[1],p2[1]])
+    plt.axis('equal')
+    plt.show()
+    return listp
+    
+def mesh_polygon(n,c1=1,c2=1,dec=False):
+    
+    polyg_p=regular_polygon(n,c1,c2)
+    center=[c1,c2]
+    listp=[]
+    for i in range (n):
+        p1=polyg_p[i]
+        if(i!=n-1):
+            p2=polyg_p[i+1]
+        else:
+            p2=polyg_p[0]
+        plt.plot([p1[0],p2[0]],[p1[1],p2[1]])
+        listp.append((p1,p2))
+        plt.plot([p1[0],center[0]],[p1[1],center[1]])
+        listp.append((p1,center))
+    plt.axis('equal')
+    plt.show()
+    return listp
+
+print(mesh_polygon(10))
 
 class GeneratePolygon(object):
 
@@ -214,4 +292,21 @@ class GeneratePolygon(object):
     def __init__(self):#constructeur de la classe
         return
         
-        
+    def regular_polygon(self,n,r=1,c1=1,c2=1):
+        listp=[]
+        for i in range(n):
+            listp.append([r*cos(2*pi*i/n)+c1,r*sin(2*pi*i/n)+c2])
+            
+#        for i in range (n):
+#            p1=listp[i]
+#            if(i!=n-1):
+#                p2=listp[i+1]
+#            else:
+#                p2=listp[0]
+#            plt.plot([p1[0],p2[0]],[p1[1],p2[1]])
+#        plt.axis('equal')
+#        plt.show()
+        return listp
+    
+#polygon=GeneratePolygon()
+#print(polygon.regular_polygon(10,r=1,c1=4,c2=4))
