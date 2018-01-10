aa8eebba5d0096c23743e2c2b605fea3b5bf336a
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-01-10 20:15:08 +0100

    voisin correction

--- Fusion_de_maillages/neighbors.py
+++ Fusion_de_maillages/neighbors.py
@@ -15,7 +15,7 @@ elem3 = [[1,2,7,3],
 
 #(nodes1, elems1, n_nodes_1, n_elems_1) = mshToPython_triangle("maillage1.msh")
 #(nodes2, elems2, n_nodes_2, n_elems_2) = mshToPython_triangle("maillage2.msh")
-elems1=[[1, 1, 5, 9], [2, 1, 8, 9], [3, 5, 6, 9], [4, 2, 5, 6], [5, 3, 6, 9], [6, 3, 7, 9], [7, 7, 8, 9], [8, 4, 7, 8]]
+elems1=[[1, 1, 5, 9], [2, 1, 8, 9], [10, 5, 6, 9], [4, 2, 5, 6], [5, 3, 6, 9], [6, 3, 7, 9], [7, 7, 8, 9], [8, 4, 7, 8]]
 
 def list_neighbors(triangles):
     """Fonction de  construction de dictionaire de voisin
@@ -26,7 +26,9 @@ def list_neighbors(triangles):
     -d:dictionaire de voisins
     """
     d = dict()
+    maximum=0
     for x in triangles:
+        maximum=max(x[0],maximum)
         x[1:]=sorted(x[1:],key=int)
         #print(x)
         t = (x[1],x[2])
@@ -41,9 +43,9 @@ def list_neighbors(triangles):
         d[t].append(x[0]) 
         d[s].append(x[0]) 
         d[r].append(x[0]) 
-    #print(d)
-    num_triangles=len(triangles)
-    return num_triangles,d
+    print(maximum)
+    #num_triangles=len(triangles)
+    return maximum,d
 print(elems1)
 n,d=list_neighbors(elems1)	
 print(d)
@@ -60,7 +62,7 @@ def neighbours(elem):
     """
 #	cette fonction est pour le maillage conforme
     n,dx=list_neighbors(elems1)
-    a = [[]]*(len(elem)+1)
+    a = [[]]*(n+1)
 #    print("len",len(elem))
 #    for i in range(1,len(elem)) :
 #        a[i] = [i]
