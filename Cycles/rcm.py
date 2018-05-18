a830459666379aefbf4c6f1cb3c613d577163bb9
HakamSophia <hakam.sophia@hotmail.fr>
2018-05-18 23:46:21 +0200

    Update rcm.py

--- Cycles/rcm.py
+++ Cycles/rcm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 ===
 Rcm
@@ -15,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-node = [[0,0,0],[0,1,0],[1,1,0],[1,0,0],[0,1,1],[1,1,1],[0,0,1],[1,0,1]]
+node = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
 x = []
 y = []
 z = []
@@ -23,12 +24,28 @@ for i in range(len(node)):
 	x.append(node[i][0])
 	y.append(node[i][1])
 	z.append(node[i][2])
-xv, yv = np.meshgrid(y, x)
+G = nx.Graph()
+for i in range(len(node)):
+	G.add_node(i, x=x[i], y=y[i], z=z[i])
+# aretes:
+G.add_edge(3,0)
+G.add_edge(0,1)
+G.add_edge(2,1)
+G.add_edge(3,2)
+G.add_edge(1,3)
+G.add_edge(1,4)
+G.add_edge(5,4)
+G.add_edge(2,5)
+G.add_edge(4,2)
+G.add_edge(4,6)
+G.add_edge(7,6)
+G.add_edge(5,7)
+G.add_edge(6,5)
+G.add_edge(6,0)
+G.add_edge(7,3)
+G.add_edge(0,7)
+print(G.nodes(data=True))
 
-# build low-bandwidth numpy matrix
-G = nx.grid_2d_graph(len(node), len(node))
-ordering = [(y,len(node)-1-x) for y in range(len(node)) for x in range(len(node))]                      
-labels = dict(zip(ordering, range(len(ordering)))) 
 nx.draw(G)
 plt.show()
 rcm = list(reverse_cuthill_mckee_ordering(G))
